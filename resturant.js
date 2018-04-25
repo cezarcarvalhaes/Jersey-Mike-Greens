08dc43dbe17a91915be758e2be66e24677190844
Nathan Geckle <nathan_geckle@yahoo.com>
2018-04-24 21:10:32 -0400

    Added the push to table thingamajiggy

--- resturant.js
+++ resturant.js
@@ -13,7 +13,7 @@ var Guest = function(name, phone, email, id){
     this.phone = phone;
     this.email = email;
     this.id = id;
-}
+};
 
 
 
@@ -23,40 +23,40 @@ app.use(bodyParser.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/index", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+  res.sendFile(path.join(__dirname, "index.html"));
+});
   
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "reservation.html"));
-  });
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "reservation.html"));
+});
 
-  app.get("/view", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
-  });
+app.get("/view", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
   
-  //Holds guest list
-  var guestList =[];
+//Holds guest list
+var guestList =[];
+
+$("#add-btn").on("click", function(event) {
+  event.preventDefault();
+  var newGuess = {
+    name: $("#name").val().trim(),
+    phone: $("#phone").val().trim(),
+    email: $("#email").val().trim(),
+    id: $("#id").val().trim(),
   
-  $("#add-btn").on("click", function(event) {
-    event.preventDefault();
-    var newGuess = {
-      name: $("#name").val().trim(),
-      phone: $("#phone").val().trim(),
-      email: $("#email").val().trim(),
-      id: $("#id").val().trim(),
-    
-      //Function to add guest to the end of list
-    addGuest = function(name, phone, email, id){
-      guestList.push(new Guest(name, phone, email, id));
-    }
-  };
+    //Function to add guest to the end of list
+  addGuest: function(name, phone, email, id){
+    guestList.push(new Guest(name, phone, email, id));
+  }
+};
 
-    
+  
 
-    
-    $.post("/api/characters", newCharacter)
-      .then(function(data) {
-        console.log("add.html", data);
-        alert("Adding character...");
-      });
-  });
+  
+  $.post("/api/characters", newCharacter)
+    .then(function(data) {
+      console.log("add.html", data);
+      alert("Adding character...");
+    });
+});
